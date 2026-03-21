e4aa113d4f93b7a5d1b609200f24249459b16fee
Solo-Okyere <quexipapphlicker@gmail.com>
2026-03-21 20:01:41 +0000

    commit

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text",{
+    strings: ["Web Developer", "Frontend Developer", "Backend Developer","Fullstack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
